c8c5338f27c87a3406f620359867afd5d573b144
Fulvio Moya <fulvio.127@gmail.com>
2023-09-20 15:40:39 -0400

    Initial Commit

---
+++ DemoTestKoombea/DemoTestKoombeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestKoombeaApp.swift
+//  DemoTestKoombea
+//
+//  Created by Fulvio A. Moya on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoTestKoombeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
